1bd1af8e7eb3e5d30f5f5f7fc217997479ff7000
Floris Heinen <florisheinen1@gmail.com>
2025-06-30 17:04:29 +0200

    Add more types and deserialization methods

--- src/deserialization.rs
+++ src/deserialization.rs
@@ -1,10 +1,12 @@
+use std::any::type_name;
+
 use chrono::NaiveDateTime;
 use serde::{Deserialize, de::Error};
 
 use crate::types::*;
 
 
-impl<'de, T> Deserialize<'de> for ApiResponse<T>
+impl<'de, T> Deserialize<'de> for ApiResponseBody<T>
 where T: Deserialize<'de>
 {
 	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
@@ -17,7 +19,7 @@ where T: Deserialize<'de>
 			let errors: Result<Vec<ApiErrorDescription>, _> = serde_path_to_error::deserialize(errors);
 			
 			match errors {
-				Ok(errors) => return Ok(ApiResponse::Err(errors)),
+				Ok(errors) => return Ok(ApiResponseBody::Err(errors)),
 				Err(parse_error) => {
 					return Err(D::Error::custom(format!("Errors: {parse_error}")));
 				},
@@ -26,7 +28,7 @@ where T: Deserialize<'de>
 		
 		let content: Result<T, _> = serde_path_to_error::deserialize(root);
 		match content {
-			Ok(content) => return Ok(ApiResponse::Ok(content)),
+			Ok(content) => return Ok(ApiResponseBody::Ok(content)),
 			Err(parse_error) => return Err(D::Error::custom(format!("Response: {parse_error}"))),
 		}
 	}
@@ -38,6 +40,8 @@ where T: Deserialize<'de> {
 	where
 		D: serde::Deserializer<'de> {
 		let root = serde_json::Value::deserialize(deserializer).expect("Failed to parse JSON");
+
+		
 
 		// Get pagination details
   		let pagination_value = root.get("Pagination").expect("Failed here");
@@ -73,7 +77,7 @@ where T: Deserialize<'de> {
 		let response = match response {
 			Ok(parse_success) => parse_success,
 			Err(parse_error) => {
-				println!("Error with parsing. Path: {}, error: {}", parse_error.path(), parse_error);
+				println!("Parsing of {} failed. Path: {}, error: {}", type_name::<T>(), parse_error.path(), parse_error);
 				return Err(D::Error::custom("Some error happened. Look above"));
 			},
 		};
@@ -87,6 +91,126 @@ where T: Deserialize<'de> {
 		Ok(Self(single_data))
 	}
 }
+
+impl<'de> Deserialize<'de> for Installation {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'de> {
+
+		let root = serde_json::Value::deserialize(deserializer).expect("Failed to parse to JSON");
+
+		let binding = root.get("Response")
+			.expect("No 'Response' field in Installation")
+			.clone();
+  		let response_field = binding
+			.as_array()
+			.expect("'Response' field was not an array");
+
+		let mut response_iter = response_field.iter();
+
+		let id: BunqId = match serde_path_to_error::deserialize(response_iter.next()
+			.expect("Not enough elements in Installation response")
+			.get("Id")
+			.expect("No 'Id' in Installation response")) {
+			Ok(id) => id,
+			Err(er) => {
+				println!("Failed to parse: {er}");
+				return Err(D::Error::custom("Failed to parse Installation"));
+			},
+		};
+
+		let token_value = response_iter.next()
+			.expect("Not enough elements in installation response 2")
+			.get("Token")
+			.expect("No 'Token' object in Installation response");
+
+		let token: InstallationToken = match serde_path_to_error::deserialize(token_value) {
+			Ok(token) => token,
+			Err(e) => {
+				println!("Failed to parse: {e}");
+				return Err(D::Error::custom("Failed to parse Installation"));
+			},
+		};
+
+		let bunq_public_key = response_iter.next()
+			.expect("Not enough elements in installation response 3")
+			.get("ServerPublicKey")
+			.expect("No 'ServerPublicKey' in Response elements of Installation")
+			.get("server_public_key").expect("No actual key")
+			.as_str().expect("Failed to parse this to string").to_string();
+
+		Ok(Self {
+			id,
+			token,
+			bunq_public_key,
+		})
+	}
+}
+
+impl<'de> Deserialize<'de> for DeviceServerSmall {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'de> {
+		
+		let root = serde_json::Value::deserialize(deserializer).map_err(|e| D::Error::custom(format!("Failed: {e}")))?;
+
+		let id = root.get("Id")
+			.ok_or_else(|| D::Error::custom("No 'Id' available in DeviceServerSmall"))?
+			.get("id")
+			.ok_or_else(|| D::Error::custom("No 'id' in Id of DeviceServerSmall"))?
+			.as_u64()
+			.ok_or_else(|| D::Error::custom("Invalid type of 'id' in DeviceServerSmall"))?
+			as u32;
+
+		Ok(DeviceServerSmall { id: id })
+	}
+}
+
+impl<'de> Deserialize<'de> for Session {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'de> {
+		let root = serde_json::Value::deserialize(deserializer)
+			.map_err(|e| D::Error::custom(format!("Failed: {e}")))?;
+
+		let response_elements = root.get("Response")
+			.ok_or_else(|| D::Error::custom("No 'Response' in Session response"))?
+			.as_array()
+			.ok_or_else(|| D::Error::custom("Response field in Session was not an array"))?;
+		let mut response_iter = response_elements.into_iter();
+
+		let id = response_iter.next()
+			.ok_or_else(|| D::Error::custom("Not enough elements in Session for Id"))?
+			.get("Id")
+			.ok_or_else(|| D::Error::custom("First element in Session did not have 'Id'"))?
+			.get("id")
+			.ok_or_else(|| D::Error::custom("Id in session did not have 'id'"))?
+			.as_u64()
+			.ok_or_else(|| D::Error::custom("'id' in Id in Session was invalid type"))?
+			as u32;
+
+		let token = serde_path_to_error::deserialize(
+			response_iter.next()
+				.ok_or_else(|| D::Error::custom("Not enough elements for 'Token' in Session"))?
+				.get("Token")
+				.ok_or_else(|| D::Error::custom("'Token' was not second element in Session response"))?
+		).map_err(|e| D::Error::custom(format!("Failed to parse Token in Session: {e}")))?;
+
+		let user_person = serde_path_to_error::deserialize(
+			response_iter.next()
+				.ok_or_else(|| D::Error::custom("Not enough elements in Session for UserPerson"))?
+				.get("UserPerson")
+				.ok_or_else(|| D::Error::custom("Third element in Session did not have 'UserPerson'"))?
+		).map_err(|e| D::Error::custom(format!("Failed to parse UserPerson in Session: {e}")))?;
+
+		Ok(Session {
+			id,
+			token,
+			user_person,
+		})
+	}
+}
+
 
 // impl<'de> Deserialize<'de> for BunqMeTabWrapper {
 // 	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
--- src/types.rs
+++ src/types.rs
@@ -1,23 +1,23 @@
 use std::ops::Deref;
 
 use chrono::NaiveDateTime;
-use serde::{de::Error, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
 use crate::deserialization::deserialize_date;
 
 ////// General Api Response types ///////
 
 #[derive(Debug, Serialize, Clone)]
-pub enum ApiResponse<T> {
+pub enum ApiResponseBody<T> {
 	Ok(T),
 	Err(Vec<ApiErrorDescription>)
 }
 
-impl<T> ApiResponse<T> {
+impl<T> ApiResponseBody<T> {
 	pub fn into_result(self) -> Result<T, Vec<ApiErrorDescription>> {
 		match self {
-			ApiResponse::Ok(v) => Ok(v),
-			ApiResponse::Err(api_error_descriptions) => Err(api_error_descriptions),
+			ApiResponseBody::Ok(v) => Ok(v),
+			ApiResponseBody::Err(api_error_descriptions) => Err(api_error_descriptions),
 		}
 	}
 }
@@ -56,9 +56,14 @@ impl<T> Deref for Single<T> {
 
 ///////////// Installation ////////////////
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CreateInstallation {
+	pub client_public_key: String
+}
+
+#[derive(Debug)]
 pub struct Installation {
-	pub id: u32,
+	pub id: BunqId,
 	pub token: InstallationToken,
 	pub bunq_public_key: String,
 }
@@ -73,7 +78,20 @@ pub struct InstallationToken {
 	pub token: String,
 }
 
+#[derive(Debug, Deserialize)]
+pub struct BunqId {
+	pub id: u32
+}
+
 ///////////// Device Server ////////////
+
+#[derive(Debug, Serialize)]
+pub struct CreateDeviceServer {
+	#[serde(rename = "secret")]
+	pub bunq_api_key: String,
+	pub description: String,
+	pub permitted_ips: Vec<String>,
+}
 
 #[derive(Debug, Deserialize)]
 pub struct DeviceServer {
@@ -87,12 +105,121 @@ pub struct DeviceServer {
 	pub status: DeviceServerStatus
 }
 
+#[derive(Debug)]
+pub struct DeviceServerSmall {
+	pub id: u32
+}
+
 #[derive(Debug, Deserialize)]
 pub enum DeviceServerStatus {
 	Active,
 	Blocked,
 	NeedsConfirmation,
 	Obsolete
+}
+
+///////////// Session ////////////
+
+#[derive(Debug, Serialize)]
+pub struct CreateSession {
+	#[serde(rename = "secret")]
+	pub bunq_api_key: String
+}
+
+#[derive(Debug)]
+pub struct Session {
+	pub id: u32,
+	pub token: SessionToken,
+	pub user_person: UserPerson,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SessionToken {
+	pub id: u32,
+	#[serde(deserialize_with = "deserialize_date")]
+	pub created: NaiveDateTime,
+	#[serde(deserialize_with = "deserialize_date")]
+	pub updated: NaiveDateTime,
+	pub token: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UserPerson {
+	pub id: u32,
+	// pub status: UserStatus,
+	#[serde(deserialize_with = "deserialize_date")]
+	pub created: NaiveDateTime,
+	#[serde(deserialize_with = "deserialize_date")]
+	pub updated: NaiveDateTime,
+	pub public_uuid: String,
+	pub session_timeout: i32,
+	// pub daily_limit_without_confirmation_login: Amount,
+	pub legal_name: String,
+	pub public_nick_name: String,
+	pub display_name: String,
+	pub first_name: String,
+	pub last_name: String,
+	pub middle_name: String,
+	// pub address_main: Address,
+	pub date_of_birth: String,
+	pub nationality: String,
+	// pub alias: Vec<BunqPointer>,
+	// pub sub_status: UserSubStatus,
+	// pub avatar: Avatar,
+	// pub notification_filters: Vec<NotificationFilter>,
+}
+
+
+////////////////// UserListing ////////////////
+
+#[derive(Debug, Deserialize)]
+pub struct User {
+	#[serde(rename = "UserPerson")]
+	pub user_person: UserPerson
+}
+
+////////////////// Monetary Account ////////////////
+#[derive(Debug, Deserialize)]
+pub struct MonetaryAccountBankWrapper {
+	#[serde(rename = "MonetaryAccountBank")]
+	pub monetary_account_bank: MonetaryAccountBank
+}
+impl Deref for MonetaryAccountBankWrapper {
+	type Target = MonetaryAccountBank;
+
+	fn deref(&self) -> &Self::Target {
+		&self.monetary_account_bank
+	}
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MonetaryAccountBank {
+	pub currency: String,
+	pub id: u32,
+	pub balance: Amount,
+	pub description: String,
+	pub status: MonetaryAccountBankStatus,
+	// pub alias: Vec<BunqPointer>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Amount {
+	pub value: String,
+	pub currency: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum MonetaryAccountBankStatus {
+	#[serde(rename = "ACTIVE")]
+	Active,
+	#[serde(rename = "BLOCKED")]
+	Blocked,
+	#[serde(rename = "CANCELLED")]
+	Cancelled,
+	#[serde(rename = "PENDING_REOPEN")]
+	PendingReopen,
+	#[serde(other)]
+	Unknown, // Catch for new status we do not know yet
 }
 
 ////////////////// BunqMeTab ////////////////
